50cda38807f35d2358b4cb80032ceab579df2f64
Synqat <ken@slinky.link>
2023-11-20 12:02:35 -0700

    s

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,34 +1,24 @@
-import {FC, PropsWithChildren, Suspense} from 'react'
-import {Header} from 'src/components/Header'
-import {Footer} from 'src/components/Footer'
-import {WebHeader} from "../components/WebHeader/index";
-import {Box, Flex, VStack, Text} from "@chakra-ui/layout";
-import {Navigation} from "../components/Navigation/index";
-import {useMediaQuery} from "@chakra-ui/react";
-import {Head} from "next/document";
+import { Box, Flex, Text, VStack } from '@chakra-ui/layout'
+import { useMediaQuery } from '@chakra-ui/react'
+import { FC, PropsWithChildren, Suspense } from 'react'
+import { Footer } from 'src/components/Footer'
+import { Header } from 'src/components/Header'
+import { Navigation } from 'src/components/Navigation'
 
-export interface LayoutProps extends PropsWithChildren {
-}
+export interface LayoutProps extends PropsWithChildren {}
 
-export const Layout: FC<LayoutProps> = ({children}) => {
-
+export const Layout: FC<LayoutProps> = ({ children }) => {
   const [isMobile] = useMediaQuery('(max-width: 1280px)')
 
-
   return (
-      <VStack w="100%" spacing={0}>
-        <Flex w={isMobile ? '100%' : '800px'}>
-          {isMobile && <Header/>}
-          <Box w={'80%'}>
-            <Suspense fallback={<Text>LOADING</Text>}>
-              {children}
-            </Suspense>
-          </Box>
-          <Box>
-            {isMobile ? <Footer/> : <Navigation/>}
-          </Box>
-        </Flex>
-      </VStack>
+    <VStack w="100%" spacing={0}>
+      <Flex w={isMobile ? '100%' : '800px'}>
+        {isMobile && <Header />}
+        <Box w={'80%'}>
+          <Suspense fallback={<Text>LOADING</Text>}>{children}</Suspense>
+        </Box>
+        <Box>{isMobile ? <Footer /> : <Navigation />}</Box>
+      </Flex>
+    </VStack>
   )
 }
-
