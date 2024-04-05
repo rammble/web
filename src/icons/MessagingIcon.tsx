e37cfd75fa8cb963c1344107ae88c0023a9e86b7
676943 <Xignotic84@gmail.com>
2024-04-05 14:46:28 +0200

    Remove deprecated/unused icons

--- src/icons/MessagingIcon.tsx
+++
@@ -1,7 +0,0 @@
-import { createIcon } from '@chakra-ui/icon'
-
-export const MessagingIcon = createIcon({
-  displayName: 'MessagingIcon',
-  viewBox: '0 0 24 24',
-  d: 'M17.2 4V11C17.2 12.1 16.3 13 15.2 13H6.19995L2.19995 17V4C2.19995 2.9 3.09995 2 4.19995 2H15.2C16.3 2 17.2 2.9 17.2 4ZM19.2 6H20.2C21.3 6 22.2 6.9 22.2 8V22L18.2 18H8.19995C7.09995 18 6.19995 17.1 6.19995 16V15H18.2C18.75 15 19.2 14.55 19.2 14V6Z',
-})
