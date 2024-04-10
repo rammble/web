a7c768d45e13711483f40faa2ee83d027ea30665
Synqat <ken@slinky.link>
2024-04-10 12:20:54 -0600

    fix merges

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -4,7 +4,7 @@ import { LogoIcon } from 'src/icons/LogoIcon'
 import { Link } from '@chakra-ui/next-js'
 import { Button, IconButton } from '@chakra-ui/button'
 import { DiscordIcon } from 'src/icons/DiscordIcon'
-import { Divider } from '@chakra-ui/react'
+import { Divider, Icon } from '@chakra-ui/react'
 import { Input } from '@chakra-ui/input'
 import { GoogleIcon } from 'src/icons/GoogleIcon'
 import { AuthLayout } from 'src/layouts/AuthLayout'
@@ -102,7 +102,7 @@ const RegisterPage: FC = () => {
                 colorScheme="neutral"
                 size="3"
                 aria-label="Discord"
-                icon={<Cross1Icon />}
+                icon={<Icon as={Cross1Icon} boxSize="18px" />}
               />
               <IconButton
                 w="full"
@@ -110,7 +110,7 @@ const RegisterPage: FC = () => {
                 colorScheme="neutral"
                 size="3"
                 aria-label="Discord"
-                icon={<MagnifyingGlassIcon />}
+                icon={<Icon as={MagnifyingGlassIcon} boxSize="18px" />}
               />
             </HStack>
             <HStack spacing={2} w="full">
